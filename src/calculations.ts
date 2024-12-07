a7f40c63ce4a5c644a8c8bed2217c7277def0370
Anil khand <anilkhandthakuri@gmail.com>
2024-12-07 19:55:48 +1100

    Gross Profit margin is calculated

--- src/calculations.ts
+++ src/calculations.ts
@@ -4,10 +4,18 @@ export const calculateRevenue = (data: any[]): number => {
         .reduce((sum, item) => sum + item.total_value, 0);
 };
 
-
-
 export const calculateExpenses = (data: any[]): number => {
     return data
         .filter(item => item.account_category === 'expense')
         .reduce((sum, item) => sum + item.total_value, 0);
+};
+
+export const calculateGrossProfitMargin = (data: any[], revenue: number): number => {
+    const sales = data
+        .filter(
+            item =>
+                item.account_type === 'sales' && item.value_type === 'debit'
+        )
+        .reduce((sum, item) => sum + item.total_value, 0);
+    return sales / revenue;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { readData, formatCurrency, formatPercentage } from './utils';
 import {
     calculateRevenue,
-    calculateExpenses
+    calculateExpenses,
+    calculateGrossProfitMargin
 
 } from './calculations';
 
 const data = readData('./data.json');
+console.log(data);
+
 
 const revenue = calculateRevenue(data);
 const expenses = calculateExpenses(data);
+const grossProfitMargin = calculateGrossProfitMargin(data, revenue);
 
 console.log(`Revenue: ${formatCurrency(revenue)}`);
 console.log(`Expenses: ${formatCurrency(expenses)}`);
+console.log(`Gross Profit Margin: ${formatPercentage(grossProfitMargin)}`);
